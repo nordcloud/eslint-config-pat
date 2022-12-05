53a827ddc30a2b848aec26002aa5550e6d61acfe
nowyDEV <dominik.nowik@nordcloud.com>
2022-12-05 14:22:49 +0100

    add graphql plugin, add profiles, list rules

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
-// See LICENSE in the project root for license information.
-
 throw new Error(
   "The index.js entry point has been removed. Please update your ESLint configuration to import one of the" +
     ' profile paths such as "@nordcloud/eslint-config-pat/profile/web-app" or "@nordcloud/eslint-config-pat/profile/node.' +
